9430bcf3cbbf99a82e25a59cee51d948289e3edd
Onaracs <brendan.scarano@gmail.com>
2016-05-18 12:58:52 -0400

    refactored code to wait for all images to be converted to base64 before sending the entire array over

--- src/Components/messages/InboxItem.js
+++ src/Components/messages/InboxItem.js
@@ -10,6 +10,7 @@ import React, {
 
 import Emoji from 'react-native-emoji';
 import NativeModules from 'NativeModules';
+import _ from 'lodash';
 
 const InboxItem = React.createClass({
 
@@ -27,89 +28,122 @@ const InboxItem = React.createClass({
     }
   },
 
+  convertPhoto(imageBase64) {
+    return new Promise((resolve) => {
+      const extLocation = imageBase64.search('ext=');
+
+      // ie: 'png, jpg, etc'
+      const extType = imageBase64.slice(extLocation, imageBase64.length).replace('ext=', '');
+
+      let base64String;
+
+      NativeModules.ReadImageData.readImage(imageBase64, (base64String) => {
+        base64String = base64String;
+        resolve({ext: extType, base64String: base64String});
+      })
+
+    });
+  },
+
   changeValue(value) {
-
     this.setState({trueSwitchIsOn: value})
-
-    // new Firebase('https://gimmie.firebaseio.com/requests/-KEdD0ahhPR-EAXec0sE').once('value', function(snap) {
-    //    console.log(snap.val());
-    // });
 
     const ref = new Firebase(`https://gimmie.firebaseio.com/requests/${this.props.id}`);
 
-    if(value) {
-
+    if (value) {
       ref.child('accepted').set(true);
 
-      // CameraRoll.getPhotos({first: 10}).then((data) => {
-      //   console.log(data);
-      //   // console.log(data.edges[0].node.image.uri);
+      CameraRoll.getPhotos({first: 2}).then((data) => {
+        const allData = [];
 
-      //   const obj = {
-      //     method: 'POST',
-      //     headers: {
-      //       'Accept': 'application/json',
-      //       'Content-Type': 'application/json',
-      //     },
-      //     body: JSON.stringify({
-      //       'data': data
-      //     })
-      //   }
+        data.edges.forEach((dataNode, index) => {
 
-      //   fetch('http://localhost:3000/firebaseNewSave', obj)
-      //     .then((res) => {
-      //       console.log(res);
-      //     })
-      // })
+          this.convertPhoto(dataNode.node.image.uri)
+            .then((resolve) => {
+              allData.push(resolve);
+              if (allData.length === data.edges.length) {
+                console.log(allData)
+                const obj = {
+                  method: 'POST',
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    'imgArray': allData,
+                    'requestId': this.props.id,
+                  })
+                }
+
+                fetch('http://localhost:3000/firebaseSaveImg', obj)
+                  .then((res) => {
+                    console.log(res);
+                  });
+              }
+
+            });
+        });
 
 
-      CameraRoll.getPhotos({first: 5}).then((data) => {
-        console.log(data);
-        console.log(data.edges[0].node.image.uri);
 
-        for (let i = 0; i < data.edges.length; i++) {
-
-          const imgDataString = data.edges[i].node.image.uri;
-
-          NativeModules.ReadImageData.readImage(imgDataString, (imageBase64) => {
-
-            const extLocation = imgDataString.search('ext=');
-
-            // ie: 'png, jpg, etc'
-            const extType = imgDataString.slice(extLocation, imgDataString.length).replace('ext=', '');
-
-            console.log(extType);
-
-            // Using the full base64 image
-            const encodeBase64data = encodeURIComponent(imageBase64);
-
-            const obj = {
-              method: 'POST',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                'img': encodeBase64data,
-                'extension': extType
-              })
-            }
-
-            fetch('http://localhost:3000/firebaseSaveImg', obj)
-              .then((res) => {
-                console.log(res);
-              })
-
-          })
-        }
-      })
+      }); // CameraRoll.getPhotos
 
     } else {
-
       ref.child('accepted').set(false);
-
     }
   },
+
+  // changeValue(value) {
+  //   this.setState({trueSwitchIsOn: value})
+
+  //   const ref = new Firebase(`https://gimmie.firebaseio.com/requests/${this.props.id}`);
+
+  //   if (value) {
+  //     ref.child('accepted').set(true);
+
+  //     CameraRoll.getPhotos({first: 2}).then((data) => {
+  //       // console.log(this.photosToBase64(data));
+  //       console.log(data);
+
+  //       return _.map(data.edges, (dataNode) => {
+
+  //         const uri = dataNode.node.image.uri;
+
+  //         const extLocation = uri.search('ext=');
+
+  //         // ie: 'png, jpg, etc'
+  //         const extType = uri.slice(extLocation, uri.length).replace('ext=', '');
+
+  //         const base64String = NativeModules.ReadImageData.readImage(uri, (base64String) => {
+
+  //           const obj = {
+  //             method: 'POST',
+  //             headers: {
+  //               'Accept': 'application/json',
+  //               'Content-Type': 'application/json',
+  //             },
+  //             body: JSON.stringify({
+  //               'img': base64String,
+  //               'extension': extType,
+  //               'requestId': this.props.id,
+  //             })
+  //           }
+
+  //           fetch('http://localhost:3000/firebaseSaveImg', obj)
+  //             .then((res) => {
+  //               console.log(res);
+  //             })
+
+  //         }) // NativeModules.ReadImageData
+
+  //       }) // _.map
+
+  //     }); // CameraRoll.getPhotos
+
+  //   } else {
+  //     ref.child('accepted').set(false);
+  //   }
+  // },
 
   render() {
     return (
@@ -156,3 +190,31 @@ const styles = StyleSheet.create({
 });
 
 module.exports = InboxItem;
+
+
+    // new Firebase('https://gimmie.firebaseio.com/requests/-KEdD0ahhPR-EAXec0sE').once('value', function(snap) {
+    //    console.log(snap.val());
+    // });
+
+// photosToBase64(data) {
+//   // NEEDS TO RETURN base64Array AFTER EVERYTHING HAS LOOPED
+//   return _.map(data.edges, (dataNode) => {
+//     const imgDataString = dataNode.node.image.uri;
+
+//     // return dataNode.node.image.uri;
+//     return NativeModules.ReadImageData.readImage(imgDataString, (imageBase64) => {
+
+//       console.log('reading base64 string');
+//       const extLocation = imgDataString.search('ext=');
+
+//       // ie: 'png, jpg, etc'
+//       const extType = imgDataString.slice(extLocation, imgDataString.length).replace('ext=', '');
+
+//       // Using the full base64 image
+//       // const encodeBase64data = encodeURIComponent(imageBase64);
+//       console.log({ext: extType, base64String: imageBase64});
+
+//       return {ext: 'extType', base64String: imageBase64}
+//     })
+//   })
+// },
